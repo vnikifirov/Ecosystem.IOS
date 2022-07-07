e886e8f843081985acc175d4e1568163cf2f8b69
Vadim <39588115+vnikifirov@users.noreply.github.com>
2022-07-07 07:12:30 +0300

    Upload projects and ideas

---
+++ IOS.Swift.Views/IOS.Swift.Views/IOS_Swift_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_Swift_ViewsApp.swift
+//  IOS.Swift.Views
+//
+//  Created by Vadim Nikiforov on 6/13/22.
+//
+
+import SwiftUI
+
+@main
+struct IOS_Swift_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WelcomeScreen()
+        }
+    }
+}
